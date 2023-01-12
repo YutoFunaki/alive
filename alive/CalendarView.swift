7d10a67984355e19d7c556e3d11af42f9bacb454
YutoFunaki <kaizuka0629@gmail.com>
2023-01-12 18:49:47 +0900

    add calendarView

---
+++ alive/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  alive
+//
+//  Created by 船木勇斗 on 2023/01/12.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("This is Calendar View")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
